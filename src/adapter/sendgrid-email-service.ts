e59b75c703834479bacdce79b2fec2e599baaf7c
Ronak Mistry <ronakmistry36@gmail.com>
2022-04-10 19:42:02 -0400

    Sendgrid email adapter

--- src/adapter/sendgrid-email-service.ts
+++ src/adapter/sendgrid-email-service.ts
@@ -1,1 +1,12 @@
-export class SendgridEmailService {}
+import sendgrid from '@sendgrid/mail';
+import { EmailDto } from '../dto/email-dto';
+
+export class SendgridEmailService {
+  constructor(apiKey: string) {
+    sendgrid.setApiKey(apiKey);
+  }
+
+  async send(dto: EmailDto): Promise<void> {
+    await sendgrid.send(dto);
+  }
+}
---
+++ src/dto/email-dto.ts
@@ -0,0 +1,7 @@
+export type EmailDto = {
+  from: string;
+  html: string;
+  subject: string;
+  text: string;
+  to: string;
+};
