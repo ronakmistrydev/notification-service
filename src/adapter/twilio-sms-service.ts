5e023587085b302cc52a8310c11a9c88d91bb509
Ronak Mistry <ronakmistry36@gmail.com>
2022-04-10 19:34:18 -0400

    Twilio sms service

--- src/adapter/twilio-sms-service.ts
+++ src/adapter/twilio-sms-service.ts
@@ -1,1 +1,13 @@
-export class TwilioSmsService {}
+import twilio, { Twilio } from 'twilio';
+
+export class TwilioSmsService {
+  private client: Twilio;
+
+  constructor(private accountId: string, private authToken: string) {
+    this.client = twilio(accountId, authToken);
+  }
+
+  async send(body: string, from: string, to: string): Promise<void> {
+    await this.client.messages.create({ body, from, to });
+  }
+}
